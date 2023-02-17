3aa32a3908991856038878cac266ffd3b51ce526
Pierre Boeno <pierrecarlos1@hotmail.com>
2023-02-17 19:18:57 -0300

    corrigindo back-to-top

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-function validar() {
-    alert("CPF Invalido");
-}
-
 var btn = document.querySelector("#back-to-top");
 
 btn.addEventListener("click", function() {
     window.scrollTo(0, 0);
 });
+
+function validar() {
+
+    var input = document.getElementById("cpf-cnpj");
+
+    var cpfCnpj = input.value;
+
+    alert("CPF: " + cpfCnpj);
+}
