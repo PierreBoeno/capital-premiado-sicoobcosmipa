1bbe27da727ea1e58d2476532b6abe1b8ed038ff
Pierre Boeno <pierrecarlos1@hotmail.com>
2023-02-16 23:29:16 -0300

    correção

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 function validar() {
     alert("CPF Invalido");
 }
+
+var btn = document.querySelector("#back-to-top");
+
+btn.addEventListener("click", function() {
+    window.scrollTo(0, 0);
+});
