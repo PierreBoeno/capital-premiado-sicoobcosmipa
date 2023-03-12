10ccd19c4211ccdf5e1b3a99501017c23c14bf17
Pierre Boeno <pierrecarlos1@hotmail.com>
2023-03-12 19:02:26 -0300

    responsividade img Banner, div lgpd e div sorteio

--- src/index.js
+++ src/index.js
@@ -4,11 +4,57 @@ btn.addEventListener("click", function() {
     window.scrollTo(0, 0);
 });
 
-function validar() {
+var btnConsultar = document.querySelector('#bntConsultar');
+var cupons = document.querySelector('#search');
+var sorteados = document.querySelector('#sorteados');
+var msgErro = document.querySelector('.erro');
+
+
+
+// btnConsultar.addEventListener('click', function() {
+//     container.style.display = 'block';
+// })
+
+const inputCpfCnpj = document.querySelector('input');
+
+inputCpfCnpj.addEventListener('keypress', () => {
+    let inputlenght = inputCpfCnpj.value.length;
+
+    if (inputlenght ===  3 || inputlenght === 7) {
+        inputCpfCnpj.value += '.'
+    } else if (inputlenght === 11) {
+        inputCpfCnpj.value += '-'
+    } else if (inputlenght > 13) {
+        console.log("CNPJ");
+    }
+})
+
+
+function consultar() {
 
     var input = document.getElementById("cpf-cnpj");
 
+
     var cpfCnpj = input.value;
 
-    alert("CPF: " + cpfCnpj);
+    // alert("CPF: " + cpfCnpj);
+    if(cupons.style.display === 'block'){
+        cupons.style.display = 'none';
+    } else {
+        cupons.style.display = 'block';
+    }
+
+    if(msgErro.style.display === 'block'){
+        msgErro.style.display = 'none';
+    } else {
+        msgErro.style.display = 'block'
+    }
+}
+
+function consulta_ganhadores() {
+    if(sorteados.style.display === 'block'){
+        sorteados.style.display = 'none';
+    } else {
+        sorteados.style.display = 'block';
+    }
 }
